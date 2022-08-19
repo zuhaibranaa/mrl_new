fcbb5933bd8ede636c963eaa3a3c8b31fac1b989
zuhaib <mcf2000656@ue.edu.pk>
2022-08-20 02:10:13 +0500

    Finished Project

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,10 +1,10 @@
-<div class="card mx-2 bg-info bg-opacity-10 mb-5" style="width: 18rem;">
+<div class="card mx-2 bg-primary text-white mb-5" style="width: 18rem;">
     <div class="card-header " style="text-align: center">
         Categories
     </div>
     <ul class="list-group list-group-flush">
         @foreach(\App\Models\Category::all() as $category)
-            <li class="list-group-item" style="text-align: center;">
+            <li class="list-group-item" style="text-align: center; background-color: lightcyan">
                 <a class="x" href="{{ route('getItemsByCat',['id'=>$category->id]) }}">{{$category->name}}</a>
             </li>
         @endforeach
